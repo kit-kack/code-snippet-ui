3e70b9ee7f2f2f40a8d4d5264eb7dea98230d8d6
littledot-kit <souler2580@foxmail.com>
2023-10-15 14:24:09 +0800

    feat: 移除mermaid

--- src/js/dep/vmd-dep.js
+++ src/js/dep/vmd-dep.js
@@ -6,7 +6,7 @@ import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
 import createTodoListPlugin from '@kangc/v-md-editor/lib/plugins/todo-list/index';
 import '@kangc/v-md-editor/lib/plugins/todo-list/todo-list.css';
-import createMermaidPlugin from '@kangc/v-md-editor/lib/plugins/mermaid/cdn';
+// import createMermaidPlugin from '@kangc/v-md-editor/lib/plugins/mermaid/cdn';
 import '@kangc/v-md-editor/lib/plugins/mermaid/mermaid.css';
 import createKatexPlugin from '@kangc/v-md-editor/lib/plugins/katex/cdn';
 import createCopyCodePlugin from '@kangc/v-md-editor/lib/plugins/copy-code/index';
@@ -16,7 +16,7 @@ VMdPreview.use(githubTheme,{
     Hljs: hljs
 })
 VMdPreview.use(createTodoListPlugin())
-VMdPreview.use(createMermaidPlugin())
+// VMdPreview.use(createMermaidPlugin())
 VMdPreview.use(createKatexPlugin())
 VMdPreview.use(createCopyCodePlugin())
 
