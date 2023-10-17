c6e9a719fcbdf621811e4a4ee987dff0e0881861
littledot-kit <souler2580@foxmail.com>
2023-10-17 22:45:40 +0800

    feat: 更新版本号

--- src/js/some.js
+++ src/js/some.js
@@ -4,8 +4,8 @@ export const CtrlStr = utools.isMacOS()? 'Command':'Ctrl';
 
 export const defaultHelpSnippet = {
     id: 'default',
-    version: 'v2.4.0',
-    name: '🎉入门手册&新功能介绍🎉',
+    version: 'v2.4.1',
+    name: '🎉入门手册&新版本介绍🎉',
     desc: '右键该元素选择预览代码，懂的人已经用v打开了',
     type: 'markdown',
     code: window.preload?.readREADME_MD(),
